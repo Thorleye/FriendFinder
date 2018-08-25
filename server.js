4e37de07179720e0fcfc5a6542707e645db80528
Thorley <thorley.eric@gmail.com>
2018-08-25 17:18:59 -0400

    finished survey

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
-var bodyparser = require("body-parser");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.json());
+
