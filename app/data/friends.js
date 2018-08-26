5dd8e3c9023b599b4934bacfdcd3d732b9613dd9
Thorley <thorley.eric@gmail.com>
2018-08-25 21:13:26 -0400

    form fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,6 @@
-var friendsArray = {
+var friendsArray = [
+
+{
     name: "Ross",
     photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPi3bcKQKn7I51usDkHZb28Gt-CUWxp-Z4xJ-KnHVPaAaIh23I",
     scores: [
@@ -13,8 +15,23 @@ var friendsArray = {
         4,
         1
       ]
-  }
-
+  },{
+      name: "Rachel",
+      photo: "http://pixel.nymag.com/imgs/daily/vulture/2014/12/17/17-rachel-green-jewish.w529.h352.jpg",
+      scores: [
+        1,
+        5,
+        3,
+        2,
+        1,
+        4,
+        5,
+        4,
+        1,
+        5
+      ]
+  },
+]
 module.exports = friendsArray;
 
   
